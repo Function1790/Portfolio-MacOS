9bebfcd5ee37198e4f457622f05f4421f475dccb
TerryFX <korean1790@gmail.com>
2024-06-02 02:12:50 +0900

    프로그램 실행 기능 추가

--- src/menu.clock.js
+++ src/menu.clock.js
@@ -1,12 +1,12 @@
 const clockText = document.getElementsByClassName("menu-clock")[0];
 
-const days = "월화수목금토일";
+const days = "일월화수목금토";
 let time;
 
 setInterval(() => {
     time = new Date();
     //6월 1일(일)오후 7:34
-    clockText.innerHTML = `${time.getMonth()}월${time.getDay()}일(${days[time.getDay()]
+    clockText.innerHTML = `${time.getMonth()}월${time.getDate()}일(${days[time.getDay()]
         })`;
     clockText.innerHTML += time.getHours() < 13 ? "오전" : "오후";
     clockText.innerHTML += ` ${time.getHours() % 12}:${time.getMinutes()}`;
--- src/program.js
+++ src/program.js
@@ -1,5 +1,61 @@
-const programsHTML = document.getElementsByClassName("program")
-const programList = []
+const desktopHTML = document.getElementsByClassName("desktop")[0]
+var programsHTML = document.getElementsByClassName("program")
+var programList = []
+
+function programHTML(name, contentHTML) {
+    return `
+    <div class="program ${name}">
+                <div class="program-topbar flex-row">
+                    <div class="program-topbar-buttons flex-row">
+                        <div class="program-circle circle1"></div>
+                        <div class="program-circle circle2"></div>
+                        <div class="program-circle circle3"></div>
+                    </div>
+                    <div class="program-title center">${name}</div>
+                    <div></div>
+                </div>
+                <div class="program-content">
+                    ${contentHTML}
+                </div>
+            </div>
+    `
+}
+
+function getGithub() {
+    return programHTML('github', `
+        <div class="github-root flex-col">
+            <div class="github-head flex-row">
+                <div class="github-profile">
+                    <img src="/img/profile.png">
+                </div>
+                <div class="github-name center">Function</div>
+            </div>
+            <div class="github-container flex-col">
+                <div class="github-repository">
+                    <div class="repository-title">T-SHOP</div>
+                    <div class="repository-content">서령제 동아리 상점</div>
+                </div>
+            </div>
+        </div>
+    `)
+}
+
+function reload() {
+    programsHTML = document.getElementsByClassName("program")
+    programList = []
+    for (var i = 0; i < programsHTML.length; i++) {
+        programList.push(new Program(programsHTML[i]))
+    }
+}
+
+function runProgram(name) {
+    switch (name) {
+        case 'github':
+            desktopHTML.innerHTML += getGithub()
+            reload()
+            break
+    }
+}
 
 function onMousemove(e) {
     if (e.buttons == 1) {
@@ -23,8 +79,4 @@ class Program {
         this.element.style.top = `${y}px`
         this.pos = [x, y]
     }
-}
-
-for (var i = 0; i < programsHTML.length; i++) {
-    programList.push(new Program(programsHTML[i]))
 }
