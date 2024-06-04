b40b8b8e1ad2bff58671268a8256fc2cc83b85a0
TerryFX <korean1790@gmail.com>
2024-06-05 01:10:24 +0900

    드래그 기능 제작중

---
+++ src/desktop.js
@@ -0,0 +1,45 @@
+// 'lastFocusedProgram', 'desktopHTML' have been declared
+// in /src/program.js
+const dragRectHTML = document.getElementsByClassName('drag-rect')[0];
+var isMousedownOnBackgound = false;
+var dragStartPos = [0, 0]
+
+function setDragStartPos(x, y){
+    dragRectHTML.style.left = `${x}px`;
+    dragRectHTML.style.top = `${y}px`;
+    dragStartPos = [x, y];
+}
+
+desktopHTML.onmousedown = (event) => {
+    // 바탕화면 클릭시
+    if (event.target == desktopHTML) {
+        setDragStartPos(event.x ,event.y)
+        dragRectHTML.style.display = 'flex';
+        isMousedownOnBackgound = true;
+    }
+}
+
+desktopHTML.onmouseup = (event) => {
+    dragRectHTML.style.width = '0px';
+    dragRectHTML.style.height = '0px';
+    dragRectHTML.style.display = 'none';
+    isMousedownOnBackgound = false;
+}
+
+desktopHTML.onmousemove = (event) => {
+    if (!isMousedownOnBackgound) {
+        return;
+    }
+    var width = event.x - dragStartPos[0]
+    var height = event.y - dragStartPos[1]
+    if (width >= 0) {
+        dragRectHTML.style.width = `${width}px`;
+    }
+    else {
+        dragRectHTML.style.left = `${event.x}px`;
+        dragRectHTML.style.width = `${width}px`;
+    }
+    if (height >= 0) {
+        dragRectHTML.style.height = `${height}px`;
+    }
+}
